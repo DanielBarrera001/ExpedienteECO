d3286e887c497af5fcd6df0bbe395a307caf52d6
Daniel <barreraquezadadanielalejandro@gmail.com>
2026-02-19 21:22:02 -0600

    Primer commit

---
+++ Expediente ECO/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Expediente_ECO.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
